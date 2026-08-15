fc35952c9e0b858eeaa5f1798f6b5b87f15c3d19
Marco-Lemos <polo.lemos@gmail.com>
2026-08-15 11:28:36 -0300

    Comita o bundle da função da Vercel para evitar problema de cache do build

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import { createApp } from "../server/_core/app";
-
-// Vercel trata um Express app exportado como default aqui como uma função
-// serverless: cada requisição a /api/* é roteada para este handler.
-const app = createApp();
-
-export default app;
